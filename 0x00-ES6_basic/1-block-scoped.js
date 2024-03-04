5ab5f683d4ed543b85fd3ab0b82f84121e376b9c
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-04 19:48:27 +0100

    block

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,3 @@
-/**
- * Function to create a task block based on the input trueOrFalse value.
- *
- * @param {boolean} trueOrFalse - A boolean value to determine the task status
- * @return {Array} An array containing the task status values
- */
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;

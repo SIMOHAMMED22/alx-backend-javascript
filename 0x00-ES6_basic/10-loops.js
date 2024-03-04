8d4754880da5b72c8ec91a9c6d569f0be0831c52
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-04 22:14:01 +0100

    loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
+  for (let value of array) {
+    array[index] = appendString + value;
+    index++;
+  }
+
+  return array;
+}

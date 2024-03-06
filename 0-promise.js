a6f3213804fa2f8e2e5721257bcbb94282cacd0a
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-07 00:10:52 +0100

    0

--- 0-promise.js
+++ 0-promise.js
@@ -1,10 +1,8 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        // Simulate an API call or any asynchronous operation
         setTimeout(() => {
-            // Resolve the Promise with a dummy response
             resolve("Response from API");
-        }, 2000); // Simulating a delay of 2 seconds
+        }, 2000);
     });
 }
 

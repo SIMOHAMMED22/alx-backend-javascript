cdc9bed089bc3f892ba093f5d55b20e78b3a97a4
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-07 15:18:59 +0100

    js

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+export default function createIteratorObject(report) {
+  const arr = [];
+
+  for (const val of Object.values(report.allEmployees)) {
+    arr.push(...val);
+  }
+
+  return arr;
+}

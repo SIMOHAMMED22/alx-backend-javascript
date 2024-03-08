f8a2cb67b3dd7675bace29e24ab4ef0a71361dfb
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-08 17:36:00 +0100

    car

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+const cloneSymbol = Symbol('cloneCar');
+export default class Car {
+  constructor(brand, motor, color) {
+    if (typeof brand === 'string') this._brand = brand;
+    if (typeof motor === 'string') this._motor = motor;
+    if (typeof color === 'string') this._color = color;
+  }
+
+  [cloneSymbol]() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
+  cloneCar() {
+    return this[cloneSymbol]();
+  }
+}

ff2e87db79dcefe49dabd6127ff548abdcc7977d
SIMOHAMMED22 <simofinance22@gmail.com>
2024-03-14 15:34:39 +0000

    task 4

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+// Subject.ts
+
+namespace Subjects {
+    export class Subject {
+        protected _teacher: Teacher;
+
+        setTeacher(teacher: Teacher) {
+            this._teacher = teacher;
+        }
+    }
+}
